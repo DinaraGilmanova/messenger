3c7dce66fed6b5887f5ca203a9c0c597470c9e9b
Динара <dinara-gilmanova@mail.ru>
2021-03-15 19:01:51 +0400

    init commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  div#app.container
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class App extends Vue {}
+</script>
+
+<style lang="scss">
+@import "styles/global";
+
+.container {
+  display: flex;
+  flex-direction: row;
+  height: 100vh;
+}
+</style>
